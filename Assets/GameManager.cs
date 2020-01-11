8061426c72a40546ede661542b22cef424da4393
Song Yang <songyang1218@gmail.com>
2020-01-11 17:26:39 -0500

    Restart and quit keys

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,7 +11,24 @@ public class GameManager : MonoBehaviour
         canvas.enabled = false;
     }
 
+    private void Update(){
+        if (Input.GetKeyDown("r")){
+            Restart();
+        }
+        else if (Input.GetKeyDown("q") || Input.GetKeyDown(KeyCode.Escape)){
+            Quit();
+        }
+    }
+
     public void WinGame(){
         canvas.enabled = true;
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit(){
+        Application.Quit();
     }
 }
