2e018b564e79f4b95969df0f2d4ff5e7aad1a895
Song Yang <songyang1218@gmail.com>
2020-01-11 18:09:40 -0500

    Fix unmoving bug

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,4 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
     public Vector3 moveVector;
+
+    
+    public Canvas canvas;
+
+    private void Awake(){
+        canvas.enabled = false;
+    }
+
+    public void WinGame(){
+        canvas.enabled = true;
+    }
 }
