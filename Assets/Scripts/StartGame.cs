3aedb81567149dce9499b22a8cdc8f1ec78e6d92
Diyuse <daniel.yan.wu@gmail.com>
2020-01-12 14:40:48 -0500

    Merge all the scenes

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField]private GameObject boardObject;
+    private Board board;
+
+    private void Start()
+    {
+        board = boardObject.GetComponent<Board>();
+        board.GenerateLevel(false);
+    }
+}
